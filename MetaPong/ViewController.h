e1bcb6bf081e0edf4674baf21335c350bcf225bb
Tobias Wermuth <tobynextdoor@gmail.com>
2014-09-13 18:27:56 +0200

    Initial Commit

---
+++ MetaPong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetaPong
+//
+//  Created by Tobias Wermuth on 13/09/14.
+//  Copyright (c) 2014 tobynextdoor LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MetaPong/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MetaPong
+//
+//  Created by Tobias Wermuth on 13/09/14.
+//  Copyright (c) 2014 tobynextdoor LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
